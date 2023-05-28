cae0627f42085e79e7ff17d95f313c3972e0905b
ersygn <126493467+ersygn@users.noreply.github.com>
2023-05-28 17:57:33 +0700

    Add files via upload

---
+++ tugas_grafikline.php
@@ -0,0 +1,173 @@
+<?php
+include('koneksi2.php');
+$data = mysqli_query($koneksi2,"select * from covid19");
+while($row = mysqli_fetch_array($data)){
+$country_other[] = $row['country_other'];
+$query = mysqli_query($koneksi2,"select total_deaths, total_cases, total_recovered, active_cases, total_test from covid19 where id='".$row['id']."'");
+$row = $query->fetch_array();
+$total_deaths[] = $row['total_deaths'];
+$total_cases[] = $row['total_cases'];
+$total_recovered[] = $row['total_recovered'];
+$active_cases[] = $row['active_cases'];
+$total_test[] = $row['total_test'];
+}
+?>
+<!DOCTYPE html>
+<html>
+<head>
+<title>Membuat Grafik Menggunakan Chart JS</title>
+<script type="text/javascript" src="Chart.js"></script>
+</head>
+<body>
+	<div style="width: 700px;height: 700px">
+		<h4>Total Cases</h4>
+		<canvas id="myChart"></canvas><br>
+		<h4>Total Deaths</h4>
+		<canvas id="myChart2"></canvas><br>
+		<h4>Total Recovered</h4>
+		<canvas id="myChart3"></canvas><br>
+		<h4>Active Cases</h4>
+		<canvas id="myChart4"></canvas><br>
+		<h4>Total Test</h4>
+		<canvas id="myChart5"></canvas>
+	</div>
+
+<script>
+var ctx = document.getElementById("myChart").getContext('2d');
+var myChart = new Chart(ctx, {
+type: 'line',
+data: {
+labels: <?php echo json_encode($country_other); ?>,
+datasets: [{
+label: 'Grafik Total Cases Data 10 Negara Covid19',
+data: <?php echo json_encode($total_cases);
+
+?>,
+
+backgroundColor: 'rgba(75, 0, 130, 0.2)',
+borderColor: 'rgba(75, 0, 130,1)',
+borderWidth: 1
+}]
+},
+options: {
+scales: {
+yAxes: [{
+ticks: {
+beginAtZero:true
+}
+}]
+}
+}
+});
+
+var ctx = document.getElementById("myChart2").getContext('2d');
+var myChart = new Chart(ctx, {
+type: 'line',
+data: {
+labels: <?php echo json_encode($country_other); ?>,
+datasets: [{
+label: 'Grafik Total Deaths Data 10 Negara Covid19',
+data: <?php echo json_encode($total_deaths);
+
+?>,
+
+backgroundColor: 'rgba(165, 42, 42, 0.2)',
+borderColor: 'rgba(165, 42, 42,1)',
+borderWidth: 1
+}]
+},
+options: {
+scales: {
+yAxes: [{
+ticks: {
+beginAtZero:true
+}
+}]
+}
+}
+});
+
+var ctx = document.getElementById("myChart3").getContext('2d');
+var myChart = new Chart(ctx, {
+type: 'line',
+data: {
+labels: <?php echo json_encode($country_other); ?>,
+datasets: [{
+label: 'Grafik Total Cases Data 10 Negara Covid19',
+data: <?php echo json_encode($total_recovered);
+
+?>,
+
+backgroundColor: 'rgba(19, 100, 0, 0.2)',
+borderColor: 'rgba(19, 100, 0,1)',
+borderWidth: 1
+}]
+},
+options: {
+scales: {
+yAxes: [{
+ticks: {
+beginAtZero:true
+}
+}]
+}
+}
+});
+
+var ctx = document.getElementById("myChart4").getContext('2d');
+var myChart = new Chart(ctx, {
+type: 'line',
+data: {
+labels: <?php echo json_encode($country_other); ?>,
+datasets: [{
+label: 'Grafik Active Cases Data 10 Negara Covid19',
+data: <?php echo json_encode($active_cases);
+
+?>,
+
+backgroundColor: 'rgba(25, 25, 112, 0.2)',
+borderColor: 'rgba(25, 25, 112,1)',
+borderWidth: 1
+}]
+},
+options: {
+scales: {
+yAxes: [{
+ticks: {
+beginAtZero:true
+}
+}]
+}
+}
+});
+
+var ctx = document.getElementById("myChart5").getContext('2d');
+var myChart = new Chart(ctx, {
+type: 'line',
+data: {
+labels: <?php echo json_encode($country_other); ?>,
+datasets: [{
+label: 'Grafik Total Test Data 10 Negara Covid19',
+data: <?php echo json_encode($total_test);
+
+?>,
+
+backgroundColor: 'rgba(26, 128, 127, 0.2)',
+borderColor: 'rgba(26, 128, 127,1)',
+borderWidth: 1
+}]
+},
+options: {
+scales: {
+yAxes: [{
+ticks: {
+beginAtZero:true
+}
+}]
+}
+}
+});
+</script>
+</body>
+
+</html>
---
+++ tugas_totaldeaths_doughnut.php
@@ -0,0 +1,116 @@
+<?php
+include('koneksi2.php');
+$data = mysqli_query($koneksi2, "select * from covid19");
+while ($row = mysqli_fetch_array($data)) {
+    $country_other[] = $row['country_other'];
+    $total_deaths[] = $row['total_deaths'];
+
+}
+?>
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Total Deaths Doughnut</title>
+    <script type="text/javascript" src="Chart.js"></script>
+</head>
+
+<body>
+    <div id="canvas-holder" style="width:45%">
+        <canvas id="chart-area"></canvas>
+    </div>
+    <script>
+        var config = {
+            type: 'doughnut',
+            data: {
+                datasets: [{
+                    data: <?php echo json_encode($total_deaths);
+
+                    ?>,
+
+                    backgroundColor: [
+                        'rgb(253, 215, 3)',
+                        'rgb(188, 143, 142)',
+                        'rgb(135, 206, 250)',
+                        'rgb(240, 128, 128)',
+                        'rgb(144, 238, 144)',
+                        'rgb(119, 136, 153)',
+                        'rgb(25, 25, 112)',
+                        'rgb(219, 112, 147)',
+                        'rgb(188, 143, 142)',
+                        'rgb(250, 99, 71)'
+                    ],
+                    borderColor: [
+                        'rgb(253, 215, 3)',
+                        'rgb(188, 143, 142)',
+                        'rgb(135, 206, 250)',
+                        'rgb(240, 128, 128)',
+                        'rgb(144, 238, 144)',
+                        'rgb(119, 136, 153)',
+                        'rgb(25, 25, 112)',
+                        'rgb(219, 112, 147)',
+                        'rgb(188, 143, 142)',
+                        'rgb(250, 99, 71)'
+                    ],
+                    label: 'Presentase Data Covid19 Di 10 Negara'
+                }],
+                labels: <?php echo json_encode($country_other); ?>
+            },
+            options: {
+                responsive: true
+            }
+        };
+        window.onload = function () {
+
+            var ctx = document.getElementById('chart-area').getContext('2d');
+
+            window.myDoughnut = new Chart(ctx, config);
+        };
+        document.getElementById('randomizeData').addEventListener('click',
+            function () {
+
+                config.data.datasets.forEach(function (dataset) {
+                    dataset.data = dataset.data.map(function () {
+                        return randomScalingFactor();
+                    });
+                });
+                window.myDoughnut.update();
+            });
+        var colorNames = Object.keys(window.chartColors);
+        document.getElementById('addDataset').addEventListener('click',
+
+            function () {
+
+                var newDataset = {
+                    backgroundColor: [],
+                    data: [],
+                    label: 'New dataset ' +
+
+                        config.data.datasets.length,
+
+                };
+                for (var index = 0; index < config.data.labels.length;
+
+                    ++index) {
+
+                    newDataset.data.push(randomScalingFactor());
+                    var colorName = colorNames[index %
+
+                        colorNames.length];
+
+                    var newColor = window.chartColors[colorName];
+                    newDataset.backgroundColor.push(newColor);
+                }
+                config.data.datasets.push(newDataset);
+                window.myDoughnut.update();
+            });
+        document.getElementById('removeDataset').addEventListener('click',
+            function () {
+
+                config.data.datasets.splice(0, 1);
+                window.myDoughnut.update();
+            });
+    </script>
+</body>
+
+</html>
